2f4fb8a02cd3b9bbec0ceaba28414cd00210fc9b
omadbek <abdullayevomadbek709@gmail.com>
2024-08-05 23:20:04 +0500

    navbar qoshildi va dark modeham

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+// imports react
+import { useEffect, useState } from "react";
+
+const toggleMode = () => {
+  return localStorage.getItem("darkMode") || "light";
+};
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [theme, setTheme] = useState(toggleMode());
 
-export default Navbar
+  // toggle dark mode
+  const toggleTheme = () => {
+    const newTheme = theme === "dark-mode" ? "light" : "dark-mode";
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList = "";
+    document.body.classList.add(theme);
+    localStorage.setItem("darkMode", theme);
+  }, [theme]);
+
+  return (
+    <header className="header">
+      <div className="header-container container">
+        <div></div>
+        <div>
+          <div className="dark-btn" onClick={toggleTheme}>
+            <input
+              type="checkbox"
+              checked={theme === "dark-mode"}
+              onChange={toggleTheme} // Add onChange handler
+            />
+            <span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
